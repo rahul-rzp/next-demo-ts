112164f87cedcc9a8b994389dba28e45be71e6cc
Rahul Makhija <rahul.makhija@razorpay.com>
2022-08-01 12:40:42 +0530

    fix fetch api call error tweak docker commands add ssr page add links to common wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Link href="/" prefetch={false}>
+    CSR page
+  </Link>
+  <Link href="/ssr" prefetch={false}>
+    SSR page
+  </Link>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
---
+++ pages/ssr.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import styles from '../styles/Home.module.css'
+import Profile from './components/Profile'
+import axios from 'axios'
+
+const Home: NextPage = ({ data }: any) => {
+  console.log(data)
+  return (
+    <div className={styles.container}>
+      {!data ? <div>Loading...</div> : (
+        <>
+        {data.map((item : any) => <Profile {...item} key={item.id}/>)}
+        </>
+      ) }
+    </div>
+  )
+}
+
+export async function getServerSideProps(){
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  return {
+    props: {
+      data: response.data
+    }
+  };
+}
+
+export default Home
